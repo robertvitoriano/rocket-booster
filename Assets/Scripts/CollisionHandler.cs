c2fd5c366ede5c72e612242d0b876e08ccb2f309
Robert Vitoriano <robertvitoriano@gmail.com>
2022-08-05 12:14:35 -0300

    doesnt decrease lives when dead

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TMPro;
 
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class CollisionHandler : MonoBehaviour
 {  
-   public  int lives = 5;
+   public  int lives = 10;
    public int collisionTime = 250;
    public int dyingTime = 3000;
    public bool isColliding = false;
@@ -16,6 +17,7 @@ public class CollisionHandler : MonoBehaviour
    public AudioClip explosionSound;
    GameObject deathScreen;
    GameObject mainCamera;
+   TextMeshProUGUI  livesText;
    Color originalColor;
    MeshRenderer meshRenderer;
    AudioSource audioSource;
@@ -28,8 +30,11 @@ public class CollisionHandler : MonoBehaviour
         originalColor = meshRenderer.material.color;
         deathScreen = GameObject.FindWithTag("deathScreen");
         mainCamera = GameObject.FindWithTag("MainCamera");
+        livesText = GameObject.FindWithTag("livesText").GetComponent<TextMeshProUGUI>();
         mainCameraAudioSource = mainCamera.GetComponent<AudioSource>();
         deathScreen.SetActive(false);
+        livesText.text = "Lives: " + lives;
+
     }
     void OnCollisionEnter(Collision other) {
         switch(other.gameObject.tag) {
@@ -56,11 +61,13 @@ public class CollisionHandler : MonoBehaviour
     }
 
     void HandleCollision(){
+        if(lives > 0){
         isColliding = true;
         meshRenderer.material.color = Color.red;
         audioSource.PlayOneShot(collisionSound, 0.7f);
         lives--;
-        if(lives <= 0) {
+        livesText.text = "Lives: " + lives;
+    }else{
           if(!hasExploded){
             HandleDeath();
           }
@@ -72,9 +79,7 @@ public class CollisionHandler : MonoBehaviour
         hasExploded = true;
         meshRenderer.material.color = Color.black;
         audioSource.PlayOneShot(explosionSound, 0.7f);
-        deathScreen.SetActive(true);
         await Task.Delay(dyingTime);
-        // Show death screen
-
+        deathScreen.SetActive(true);
     }
 }
